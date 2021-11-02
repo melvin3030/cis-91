56bfd5503a7bc955ec100562a88d198eb532d154
Melvin Crane <melvincan2@gmail.com>
2021-11-02 16:42:29 -0700

    created subnets

--- lab11/main.tf
+++ lab11/main.tf
@@ -31,26 +31,44 @@ provider "google" {
   project = var.project
 }
 
-resource "google_compute_network" "vpc_network" {
-  name = "cis91-network"
+resource "google_compute_network" "lab11_vpc" {
+  name = "cis91-lab11-network"
 }
 
-resource "google_compute_instance" "vm_instance" {
-  name         = "cis91"
-  machine_type = "e2-micro"
+resource "google_compute_subnetwork" "network_10.17.0.0" {
+  name          = "lab11-subnetwork01"
+  ip_cidr_range = "10.17.0.0/21"
+  region        = "us-central1"
+  network       = google_compute_network.lab11_vpc.id
 
-  boot_disk {
-    initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-2004-lts"
-    }
-  }
+resource "google_compute_subnetwork" "network_10.17.8.0" {
+  name          = "lab11-subnetwork02"
+  ip_cidr_range = "10.17.8.0/21"
+  region        = "asia-southeast1"
+  network       = google_compute_network.lab11_vpc.id
 
-  network_interface {
-    network = google_compute_network.vpc_network.name
-    access_config {
-    }
-  }
-}
+resource "google_compute_subnetwork" "network_10.17.16.0" {
+  name          = "lab11-subnetwork03"
+  ip_cidr_range = "10.17.16.0/21"
+  region        = "australia-southeast1"
+  network       = google_compute_network.lab11_vpc.id
+
+#resource "google_compute_instance" "vm_instance" {
+  #name         = "cis91"
+  #machine_type = "e2-micro"
+
+  #boot_disk {
+    #initialize_params {
+      #image = "ubuntu-os-cloud/ubuntu-2004-lts"
+    #}
+  #}
+
+  #network_interface {
+    #network = google_compute_network.vpc_network.name
+    #access_config {
+    #}
+  #}
+#}
 
 resource "google_compute_firewall" "default-firewall" {
   name = "default-firewall"
